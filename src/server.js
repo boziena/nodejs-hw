276e91a97bb32ff02c56ecc1c9296e259d12dffb
Bozhena <bvolianska@gmail.com>
2026-05-29 22:55:56 +0200

    try nm 4

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ export const setupServer = () => {
     throw new Error("Simulated server error");
   });
 
-  app.use("*", (req, res) => {
+  app.use((req, res) => {
     res.status(404).json({
       message: "Route not found",
     });
