f240558a25612012a445c9c5adefd0bb0f93ac14
Bozhena <bvolianska@gmail.com>
2026-05-30 22:45:52 +0200

    try 6

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,20 @@
+import { isHttpError } from 'http-errors';
+
 export const errorHandler = (err, req, res, next) => {
-  const status = err.status || 500;
-  res.status(status).json({
-    message: err.message || 'Internal Server Error',
+  if (isHttpError(err)) {
+    return res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: {
+        message: err.message,
+      },
+    });
+  }
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: {
+      message: err.message,
+    },
   });
 };
