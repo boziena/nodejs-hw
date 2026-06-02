7f35628b47946b4462aa83e5d7270b3b4b4e9b84
Bozhena <bvolianska@gmail.com>
2026-06-02 21:00:35 +0200

    try 4

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,43 +1,26 @@
-import { celebrate, Joi, Segments } from 'celebrate';
-import mongoose from 'mongoose';
+import { Joi } from 'celebrate';
 import { TAGS } from '../constants/tags.js';
 
-const isValidObjectId = (value, helpers) => {
-  if (!mongoose.Types.ObjectId.isValid(value))
-    return helpers.message('Invalid id');
-  return value;
-};
-
-export const getAllNotesSchema = celebrate({
-  [Segments.QUERY]: Joi.object({
-    page: Joi.number().integer().min(1).default(1),
-    perPage: Joi.number().integer().min(5).max(20).default(10),
-    tag: Joi.string().valid(...TAGS),
-    search: Joi.string().allow(''),
-  }),
+// Важливо: експортуємо ТІЛЬКИ Joi.object({...})
+export const getAllNotesSchema = Joi.object({
+  page: Joi.number().integer().min(1).default(1),
+  perPage: Joi.number().integer().min(5).max(20).default(10),
+  tag: Joi.string().valid(...TAGS),
+  search: Joi.string().allow(''),
 });
 
-export const noteIdSchema = celebrate({
-  [Segments.PARAMS]: Joi.object({
-    noteId: Joi.string().custom(isValidObjectId).required(),
-  }),
+export const noteIdSchema = Joi.object({
+  noteId: Joi.string().hex().length(24).required(), // Або ваш кастомний валідатор ObjectId
 });
 
-export const createNoteSchema = celebrate({
-  [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1).required(),
-    content: Joi.string().allow(''),
-    tag: Joi.string().valid(...TAGS),
-  }),
+export const createNoteSchema = Joi.object({
+  title: Joi.string().min(1).required(),
+  content: Joi.string().allow(''),
+  tag: Joi.string().valid(...TAGS),
 });
 
-export const updateNoteSchema = celebrate({
-  [Segments.PARAMS]: Joi.object({
-    noteId: Joi.string().custom(isValidObjectId).required(),
-  }),
-  [Segments.BODY]: Joi.object({
-    title: Joi.string().min(1),
-    content: Joi.string().allow(''),
-    tag: Joi.string().valid(...TAGS),
-  }).min(1),
-});
+export const updateNoteSchema = Joi.object({
+  title: Joi.string().min(1),
+  content: Joi.string().allow(''),
+  tag: Joi.string().valid(...TAGS),
+}).min(1);
