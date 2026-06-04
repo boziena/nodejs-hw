184e88d4519a600e93e3c889b4157dbc9bd66340
Bozhena <bvolianska@gmail.com>
2026-06-05 00:32:59 +0200

    Fix: Add missing exports in notesValidation

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -2,11 +2,22 @@ import { Joi } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
 import { TAGS } from '../constants/tags.js';
 
+// Кастомний валідатор
 const objectIdValidator = (value, helpers) => {
   if (!isValidObjectId(value)) {
     return helpers.message('Invalid id');
   }
   return value;
+};
+
+// ОБОВ'ЯЗКОВО мають бути export
+export const getAllNotesSchema = {
+  query: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid(...TAGS),
+    search: Joi.string().allow(''),
+  }),
 };
 
 export const noteIdSchema = {
